3bafe2059eddd04f1d4bc5428a0fa9b8a086910e
Austin Rhoads <austinrrhoads@ymail.com>
2020-03-09 21:57:40 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -7,10 +7,10 @@ def create_project_hash
 html = File.read('fixtures/kickstarter.html')
 kickstarter = Nokogiri::HTML(html)
 binding.pry
-# projects: kickstarter.css("li.project.grid_4").first 
+# projects: kickstarter.css("li.project.grid_4").first
 # title: project.css("h2.bbcard_name strong a").text
 # image link: project.css("div.project-thumbnail a img").attribute("src").value
-# description: project.css("p.bbcard_blurb").text 
+# description: project.css("p.bbcard_blurb").text
 # location:  project.css("span.location-name").text
 end
 
